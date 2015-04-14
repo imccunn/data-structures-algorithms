a92866c038385aaf41b8622a513e173198266400
Ian McCunn <ianmccunn@gmail.com>
2015-04-14 13:43:34 -0700

    cleaning

--- Cake/fib.js
+++ Cake/fib.js
@@ -1,7 +1,7 @@
 // Recursive: O(2^n) --- BAD PROGRAMMER ---
 function fib(n) {
 	if (n < 2) return n;
-    return fib(n-1) + fib(n-2);
+  return fib(n-1) + fib(n-2);
 }
 
 /**
@@ -9,25 +9,25 @@ function fib(n) {
  */
 function range(n) {
 	var range = [];
-    for (var i = 1; i <= n; i++) range.push(i);
-    return range;
+  for (var i = 1; i <= n; i++) range.push(i);
+  return range;
 }
 
 // Iterative Solution: O(n) time, O(1) space ----- GOOD -----
 function fib_iterative(n) {
 	if (n < 0) throw new RangeError("Index cannot be negative.");
-    else if (n < 2) return n;
+  else if (n < 2) return n;
 
-    var prev = 0;
-    var prev_prev = 1;
-    var current;
+  var prev = 0;
+  var prev_prev = 1;
+  var current;
 
-    for (var i in range(n)) {
-    	current = prev + prev_prev;
-        prev_prev = prev;
-        prev = current;
-    }
-    return current;
+  for (var i in range(n)) {
+  	current = prev + prev_prev;
+      prev_prev = prev;
+      prev = current;
+  }
+  return current;
 }
 
 // Recursive with memoization: O(log n) time --- BEST ---
@@ -35,11 +35,11 @@ function Fib() {
 	this.memo = {};
   this.fib = function(n) {
   	if (n < 0) throw new RangeError("Cannot compute negative numbers.");
-      else if (n < 2) return n;
-      if (n in this.memo) return this.memo[n];
-      var newResult = this.fib(n-1) + this.fib(n-2);
-      this.memo[n] = newResult;
-      return newResult;
+    else if (n < 2) return n;
+    if (n in this.memo) return this.memo[n];
+    var newResult = this.fib(n-1) + this.fib(n-2);
+    this.memo[n] = newResult;
+    return newResult;
   };
 }
 
